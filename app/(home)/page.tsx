9a313f9e147f73d9937d4f44ac7f72d7c4110148
DevGisoun <devgisoun@gmail.com>
2024-07-05 18:29:12 +0900

    #3.5 Suspense

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,32 @@
-export const metadata = {
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metaData: Metadata = {
   title: 'Home',
+};
+
+export const API_URL: string = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies(): Promise<any[]> {
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
+  const json: any[] = await response.json();
+  return json;
 }
 
-export default function Page() {
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      <h1>Hello !!</h1>
+      {movies.map(
+        movie => (
+          <li key={movie.id}>
+            <Link href={`/movies/${movie.id}`}>
+              {movie.title}
+            </Link>
+          </li>
+        )
+      )}
     </div>
   );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,19 @@
-export default function MovieDetail(
+import { Suspense } from "react";
+import MovieVideos from "../../../../components/movie-videos";
+import MovieInfo from "../../../../components/movie-info";
+
+export default async function MovieDetail(
   { params: { id } }: { params: { id: string }, }
 ) {
   return (
-    <h1>Movie {id}</h1>
+    <div>
+      <h3>Movie Detail Page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
   );
 }
