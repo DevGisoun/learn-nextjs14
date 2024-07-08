43f5eff82eb72c7e910909c15ae2826e94cb525c
DevGisoun <devgisoun@gmail.com>
2024-07-08 15:30:13 +0900

    #4.5 Deployment

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,5 +1,5 @@
+import { API_URL } from '../app/constants';
 import styles from '../styles/movie-videos.module.css';
-import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string): Promise<any[]> {
   const response = await fetch(`${API_URL}/${id}/videos`);
