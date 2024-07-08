f58f331d66b2ee11c30c9acb712ee82c61b7bc82
DevGisoun <devgisoun@gmail.com>
2024-07-08 15:04:49 +0900

    #4.3 Movie Trailers

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,18 +1,26 @@
+import styles from '../styles/movie-videos.module.css';
 import { API_URL } from "../app/(home)/page";
 
-async function getVideos(id: string) {
-  console.log(`Fetching videos : ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error('something broke...');
-  // const response = await fetch(`${API_URL}/${id}/videos`);
-  // return response.json();
+async function getVideos(id: string): Promise<any[]> {
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos(
-  {id}: {id: string},
+  { id }: { id: string },
 ) {
   const videos = await getVideos(id);
   return (
-    <h6>{JSON.stringify(videos)}</h6>
+    <div className={styles.container}>
+      {videos.map(video => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelermeter; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
   );
 }
